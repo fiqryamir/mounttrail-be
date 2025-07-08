7a1a15071c8ce4ab5b57bc5ea9250cecfb356580
Muhammad Fiqry <mfiqry9907@gmail.com>
2025-07-08 12:18:55 +0800

    url

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <-- Add this line
+use Illuminate\Support\Facades\Request;
 
 class AppServiceProvider extends ServiceProvider
 {
