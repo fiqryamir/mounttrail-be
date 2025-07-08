be9e3f724c4b0e454920a8ff572c899b7b41e1ac
Muhammad Fiqry <mfiqry9907@gmail.com>
2025-07-08 12:16:15 +0800

    trust proxies

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -17,8 +17,23 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot()
     {
-        //
+        // --- ADD THE FOLLOWING CODE ---
+
+        // Check if the application is running in a production environment
+        // and force the URL to use HTTPS.
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+
+            // Set the trusted proxies to trust all proxies,
+            // which is fine for a platform like Render.
+            Request::setTrustedProxies(
+                ['*'],
+                Request::HEADER_X_FORWARDED_ALL
+            );
+        }
+
+        // --- END OF ADDED CODE ---
     }
 }
